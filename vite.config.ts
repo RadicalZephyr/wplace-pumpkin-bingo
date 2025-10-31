58bf3877df7348f8a5cb37ac4be924b27bfaec73
Zefira Shannon <zefira@hey.com>
2025-10-31 05:41:22 -0700

    Don't minify the code so it's easier to check

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/wplace-pumpkin-bingo/",
+  build: {
+    minify: false, // 👈 disable code minification
+    sourcemap: true, // optional, useful for readable output
+  },
   plugins: [
     react({
       babel: {
